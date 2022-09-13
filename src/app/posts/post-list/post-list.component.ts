3786bfeb7f8b83d8b5320c5a070af596b25ca05c
caileighsmith <caihidden@gmail.com>
2022-09-14 00:35:15 +0100

    Updating UI, adding frontend

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,24 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent{
+    posts = [
+        {
+            title: 'first Post',
+            content: 'This is the first post'
+        },
+        {
+            title: 'Second Post',
+            content: 'This is the Second post'
+        },
+        {
+            title: 'Third Post',
+            content: 'This is the Third post'
+        }
+    ];
+}
